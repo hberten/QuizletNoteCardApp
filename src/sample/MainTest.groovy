94330a2da5c4558f7545d7d660fd24b8f8303834
hberten <hberten@missouriwestern.edu>
2018-05-02 18:08:38 -0500

    Clean up.

--- src/sample/MainTest.groovy
+++ src/sample/MainTest.groovy
@@ -1,15 +1,15 @@
-//package sample
-//
-//class MainTest extends GroovyTestCase {
-//    void setUp() {
-//        super.setUp()
-//    }
-//
-//    void searchButtonClick() {
-//        String result = Main.displayNoteCardUI("https://quizlet.com/6345837/astronomy-chapter-1-4-flash-cards/");
-//        assert result != null
-//        assert result != ""
-//        assert result == Credentials.readFromURL("https://quizlet.com/6345837/astronomy-chapter-1-4-flash-cards/")
-//
-//    }
-//}
+package sample
+
+class MainTest extends GroovyTestCase {
+    void setUp() {
+        super.setUp()
+    }
+
+    void searchButtonClick() {
+        String result = Main.displayNoteCardUI("https://quizlet.com/6345837/astronomy-chapter-1-4-flash-cards/");
+        assert result != null
+        assert result != ""
+        assert result == Credentials.readFromURL("https://quizlet.com/6345837/astronomy-chapter-1-4-flash-cards/")
+
+    }
+}
